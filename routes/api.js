a153612187c5a9c67e612c5b32078b4d04a9e09a
Talhah Awan <talhahawan@gmail.com>
2022-01-13 01:14:09 -0600

    completed range router

--- routes/api.js
+++ routes/api.js
@@ -20,6 +20,21 @@ router.get("/api/workouts", (req, res) => {
 
 router.get("api/workouts/range", (req, res) => {
     Workout.aggregate([
-        
+        {
+            $addFields: {
+                totalDuration:
+                { $sum: '$exercises.duration' },
+                totalWeight:
+                { $sum: '$exercises.weight' },
+            }
+        }
     ])
+    .sort({day:-1})
+    .limit(7)
+    .then(dbWorkout => {
+        res.json(dbWorkout.reverse());
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
 });
