4ebe647aedec7119665bcd7158195b8aa226a98f
Talhah Awan <talhahawan@gmail.com>
2022-01-13 00:49:10 -0600

    started router.get for workouts

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,13 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout");
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration:
+                { $sum: '$exercises.duration' },
+            }
+        }
+    ])
+});
