165e9cd5f1e4d912ac1b17a69a29d2ffc5cc2b4a
Talhah Awan <talhahawan@gmail.com>
2022-01-13 01:08:58 -0600

    Started range router.get

--- routes/api.js
+++ routes/api.js
@@ -17,3 +17,9 @@ router.get("/api/workouts", (req, res) => {
         res.status(400).json(err);
     });
 });
+
+router.get("api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        
+    ])
+});
