773528b1a04c089b8081d9888344bbc0bd526c46
Talhah Awan <talhahawan@gmail.com>
2022-01-13 01:54:19 -0600

    added app.use express and logger

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
