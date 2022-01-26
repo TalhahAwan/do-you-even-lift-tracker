a9ea380012f8dd17014962e70717b7b606f5b2b1
Talhah Awan <talhahawan@gmail.com>
2022-01-26 02:59:07 -0600

    mongoose connection

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/do-you-even-lift-tracker", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
